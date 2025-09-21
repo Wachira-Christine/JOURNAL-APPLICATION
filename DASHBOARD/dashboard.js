2726e04d6a16643eaaf7b55ae84ee72b43241054
ShaloNatalie <natalie.shalo@strathmore.edu>
2025-09-21 23:46:07 +0300

    dashboard integration and ensure compatibility in code

--- DASHBOARD/dashboard.js
+++ DASHBOARD/dashboard.js
@@ -2,18 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const logoutBtn = document.getElementById('logoutBtn');
 
     logoutBtn.addEventListener('click', function() {
-
         // Clear all saved session data
         sessionStorage.clear();
         localStorage.removeItem('userSession');
 
-        // Redirect the user back to the login
-        window.location.href = 'login.html';
-    }
-    );
-
-
-
-
-}
-);
+        // Redirect the user back to the login page
+        window.location.href = '../Signup&Login/index.html';
+    });
+});
